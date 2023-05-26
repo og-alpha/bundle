6897c6406466d5d9cc3b97b5dc340f58defbde62
Alpha <80983100+og-alpha@users.noreply.github.com>
2023-05-25 22:04:27 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -20331,113 +20331,116 @@ var bundle = (function (exports) {
    ];
    const completionKeymapExt = /*@__PURE__*/Prec.highest(/*@__PURE__*/keymap.computeN([completionConfig], state => state.facet(completionConfig).defaultKeymap ? [completionKeymap] : []));
 
-   // Using https://github.com/one-dark/vscode-one-dark-theme/ as reference for the colors
-   const chalky = "#e5c07b", coral = "#e06c75", cyan = "#56b6c2", invalid = "#ffffff", ivory = "#abb2bf", stone = "#7d8799", // Brightened compared to original to increase contrast
-   malibu = "#61afef", sage = "#98c379", whiskey = "#d19a66", violet = "#c678dd", darkBackground = "#21252b", highlightBackground = "#2c313a", background = "#282c34", tooltipBackground = "#353a42", selection = "#3E4451", cursor = "#528bff";
-   /**
-   The editor theme styles for One Dark.
-   */
-   const oneDarkTheme = /*@__PURE__*/EditorView.theme({
-       "&": {
-           color: ivory,
-           backgroundColor: background
-       },
-       ".cm-content": {
-           caretColor: cursor
-       },
-       ".cm-cursor, .cm-dropCursor": { borderLeftColor: cursor },
-       "&.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection": { backgroundColor: selection },
-       ".cm-panels": { backgroundColor: darkBackground, color: ivory },
-       ".cm-panels.cm-panels-top": { borderBottom: "2px solid black" },
-       ".cm-panels.cm-panels-bottom": { borderTop: "2px solid black" },
-       ".cm-searchMatch": {
-           backgroundColor: "#72a1ff59",
-           outline: "1px solid #457dff"
-       },
-       ".cm-searchMatch.cm-searchMatch-selected": {
-           backgroundColor: "#6199ff2f"
-       },
-       ".cm-activeLine": { backgroundColor: "#6699ff0b" },
-       ".cm-selectionMatch": { backgroundColor: "#aafe661a" },
-       "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
-           backgroundColor: "#bad0f847"
-       },
-       ".cm-gutters": {
-           backgroundColor: background,
-           color: stone,
-           border: "none"
-       },
-       ".cm-activeLineGutter": {
-           backgroundColor: highlightBackground
-       },
-       ".cm-foldPlaceholder": {
-           backgroundColor: "transparent",
-           border: "none",
-           color: "#ddd"
-       },
-       ".cm-tooltip": {
-           border: "none",
-           backgroundColor: tooltipBackground
-       },
-       ".cm-tooltip .cm-tooltip-arrow:before": {
-           borderTopColor: "transparent",
-           borderBottomColor: "transparent"
-       },
-       ".cm-tooltip .cm-tooltip-arrow:after": {
-           borderTopColor: tooltipBackground,
-           borderBottomColor: tooltipBackground
-       },
-       ".cm-tooltip-autocomplete": {
-           "& > ul > li[aria-selected]": {
-               backgroundColor: highlightBackground,
-               color: ivory
-           }
-       }
-   }, { dark: true });
-   /**
-   The highlighting style for code in the One Dark theme.
-   */
-   const oneDarkHighlightStyle = /*@__PURE__*/HighlightStyle.define([
-       { tag: tags.keyword,
-           color: violet },
-       { tag: [tags.name, tags.deleted, tags.character, tags.propertyName, tags.macroName],
-           color: coral },
-       { tag: [/*@__PURE__*/tags.function(tags.variableName), tags.labelName],
-           color: malibu },
-       { tag: [tags.color, /*@__PURE__*/tags.constant(tags.name), /*@__PURE__*/tags.standard(tags.name)],
-           color: whiskey },
-       { tag: [/*@__PURE__*/tags.definition(tags.name), tags.separator],
-           color: ivory },
-       { tag: [tags.typeName, tags.className, tags.number, tags.changed, tags.annotation, tags.modifier, tags.self, tags.namespace],
-           color: chalky },
-       { tag: [tags.operator, tags.operatorKeyword, tags.url, tags.escape, tags.regexp, tags.link, /*@__PURE__*/tags.special(tags.string)],
-           color: cyan },
-       { tag: [tags.meta, tags.comment],
-           color: stone },
-       { tag: tags.strong,
-           fontWeight: "bold" },
-       { tag: tags.emphasis,
-           fontStyle: "italic" },
-       { tag: tags.strikethrough,
-           textDecoration: "line-through" },
-       { tag: tags.link,
-           color: stone,
-           textDecoration: "underline" },
-       { tag: tags.heading,
-           fontWeight: "bold",
-           color: coral },
-       { tag: [tags.atom, tags.bool, /*@__PURE__*/tags.special(tags.variableName)],
-           color: whiskey },
-       { tag: [tags.processingInstruction, tags.string, tags.inserted],
-           color: sage },
-       { tag: tags.invalid,
-           color: invalid },
-   ]);
-   /**
-   Extension to enable the One Dark theme (both the editor theme and
-   the highlight style).
-   */
-   const oneDark = [oneDarkTheme, /*@__PURE__*/syntaxHighlighting(oneDarkHighlightStyle)];
+   var createTheme = _ref => {
+     var {
+       theme,
+       settings = {},
+       styles = []
+     } = _ref;
+     var themeOptions = {
+       '.cm-gutters': {}
+     };
+     var baseStyle = {};
+     if (settings.background) {
+       baseStyle.backgroundColor = settings.background;
+     }
+     if (settings.foreground) {
+       baseStyle.color = settings.foreground;
+     }
+     if (settings.background || settings.foreground) {
+       themeOptions['&'] = baseStyle;
+     }
+     if (settings.fontFamily) {
+       themeOptions['&.cm-editor .cm-scroller'] = {
+         fontFamily: settings.fontFamily
+       };
+     }
+     if (settings.gutterBackground) {
+       themeOptions['.cm-gutters'].backgroundColor = settings.gutterBackground;
+     }
+     if (settings.gutterForeground) {
+       themeOptions['.cm-gutters'].color = settings.gutterForeground;
+     }
+     if (settings.gutterBorder) {
+       themeOptions['.cm-gutters'].borderRightColor = settings.gutterBorder;
+     }
+     if (settings.caret) {
+       themeOptions['.cm-content'] = {
+         caretColor: settings.caret
+       };
+       themeOptions['.cm-cursor, .cm-dropCursor'] = {
+         borderLeftColor: settings.caret
+       };
+     }
+     var activeLineGutterStyle = {};
+     if (settings.gutterActiveForeground) {
+       activeLineGutterStyle.color = settings.gutterActiveForeground;
+     }
+     if (settings.lineHighlight) {
+       themeOptions['.cm-activeLine'] = {
+         backgroundColor: settings.lineHighlight
+       };
+       activeLineGutterStyle.backgroundColor = settings.lineHighlight;
+     }
+     themeOptions['.cm-activeLineGutter'] = activeLineGutterStyle;
+     if (settings.selection) {
+       themeOptions['&.cm-focused .cm-selectionBackground, & .cm-selectionLayer .cm-selectionBackground, .cm-content ::selection'] = {
+         backgroundColor: settings.selection
+       };
+     }
+     if (settings.selectionMatch) {
+       themeOptions['& .cm-selectionMatch'] = {
+         backgroundColor: settings.selectionMatch
+       };
+     }
+     var themeExtension = EditorView.theme(themeOptions, {
+       dark: theme === 'dark'
+     });
+     var highlightStyle = HighlightStyle.define(styles);
+     var extension = [themeExtension, syntaxHighlighting(highlightStyle)];
+     return extension;
+   };
+
+   var defaultSettingsDracula = {
+     background: '#282a36',
+     foreground: '#f8f8f2',
+     caret: '#f8f8f0',
+     selection: 'rgba(255, 255, 255, 0.1)',
+     selectionMatch: 'rgba(255, 255, 255, 0.2)',
+     gutterBackground: '#282a36',
+     gutterForeground: '#6D8A88',
+     gutterBorder: 'transparent',
+     lineHighlight: 'rgba(255, 255, 255, 0.1)'
+   };
+   var dracula = createTheme({
+       theme: 'dark',
+       settings: defaultSettingsDracula,
+       styles: [{
+         tag: tags.comment,
+         color: '#6272a4'
+       }, {
+         tag: tags.string,
+         color: '#f1fa8c'
+       }, {
+         tag: tags.atom,
+         color: '#bd93f9'
+       }, {
+         tag: tags.meta,
+         color: '#f8f8f2'
+       }, {
+         tag: [tags.keyword, tags.operator, tags.tagName],
+         color: '#ff79c6'
+       }, {
+         tag: [tags.function(tags.propertyName), tags.propertyName],
+         color: '#66d9ef'
+       }, {
+         tag: [tags.definition(tags.variableName), tags.function(tags.variableName), tags.className, tags.attributeName],
+         color: '#50fa7b'
+       }, {
+         tag: tags.atom,
+         color: '#bd93f9'
+       }]
+   });
 
    function createEditorState(initialContents, options = {}) {
        let extensions = [
@@ -20467,10 +20470,8 @@ var bundle = (function (exports) {
            ]),
            options.lang,
            syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+           dracula
        ];
-
-       if (options.oneDark)
-           extensions.push(oneDark);
 
        return EditorState.create({
            doc: initialContents,
